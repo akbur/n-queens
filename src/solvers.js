bdd5371cd13ce86accaf3e3e92b23e23fa3e177e
Kris Burke <kris.burke611@gmail.com>
2016-01-21 19:28:57 -0500

    Added find solution helper function and countNRooksSolutions function

--- src/solvers.js
+++ src/solvers.js
@@ -13,11 +13,26 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+window.findSolution = function(row, n, validator, board, callback) {
+  if(row === n){
+    return callback()
+  }
+  for (var i = 0; i < n; i++) {
+    board.togglePiece(row, i);
+    if(!board[validator]()){
+      var result = findSolution(row + 1, n, validator, board, callback);
+      if (result) {
+        return result;
+      }
+    }
+    board.togglePiece(row, i)
+  }
+};
+
 window.findNRooksSolution = function(n) {
   var board = new Board({n: n});
   var rows = board.rows();
 
-  console.dir(rows);
   _.each(rows, function(row, index){
     var placed = false;  
     _.each(row, function(square, i){
@@ -40,7 +55,12 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+
+  findSolution(0, n, 'hasAnyRooksConflicts', board, function(){
+    solutionCount++;
+  });
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -59,7 +79,12 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+
+  findSolution(0, n, 'hasAnyQueensConflicts', board, function(){
+    solutionCount++;
+  });
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
